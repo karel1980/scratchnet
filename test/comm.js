7c5aea74d009ef3ec374a823f60ae766877006d5
Karel Vervaeke <karel@ngdata.com>
2014-02-23 10:30:48 +0100

    Use alice and bob instead of the rather dry comm1 and comm2

--- test/comm.js
+++ test/comm.js
@@ -15,21 +15,23 @@ function testComm(id, port1, port2, service) {
 
 describe('comm', function(){
   it('should add messages to its queue', function(done){
-    var comm1 = testComm("alice", 3333, 3334, svc_chat)
-    var comm2 = testComm("bob", 3334, 3333, svc_chat)
+    var alice = testComm("alice", 3333, 3334, svc_chat)
+    var bob = testComm("bob", 3334, 3333, svc_chat)
 
     for (var i = 0; i < 5; i++) {
-      comm1.sendMessage({'message': 'send', 'args': [ "value" + i ]})
+      alice.sendMessage({'message': 'send', 'args': [ "hi bob " + i ]})
+      bob.sendMessage({'message': 'send', 'args': [ "hi alice " + i ]})
     }
 
-    setTimeout( // comm2 receives in another thread, so using timeout
+    setTimeout( // send/receive is async, so just testing after a timeout
       function() {
         // FIXME:
         // Note: we're going under the hood here, actually we should verify the
         // result by getting /poll. (OTOH, relative merits of writing a bug-compatible
         // /poll response parser are debateable)
         for (var i = 0; i < 5; i++) {
-          assert.equal("value" + i, comm2.queues['send'][String(i+1)]['msg'])
+          assert.equal("hi alice " + i, alice.queues['send'][String(i+1)]['msg'])
+          assert.equal("hi bob " + i, bob.queues['send'][String(i+1)]['msg'])
         }
         done()
       },
