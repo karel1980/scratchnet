3495c3ab7758415a1f70b06c1f37633fb0430fe8
Marc Portier <marc.portier@gmail.com>
2014-02-28 12:08:11 +0100

    rewriting test to use allocated port rather then fixed port.

--- test/comm.js
+++ test/comm.js
@@ -4,39 +4,52 @@ svc_chat = require('./resources/config/service/chat-service.json')
 
 var assert = require("assert")
 
-function testComm(id, port1, port2, service) {
+function testComm(id, service, started) {
   return comm({
-    id: "alice",
-    bind: "localhost:" + port1,
-    connect: "localhost:" + port2,
+    id: id,
+    bind: {},
     service: service
-  })   
+  }, started)   
 }
 
 describe('comm', function(){
-  it('should add messages to its queue', function(done){
-    var alice = testComm("alice", 3333, 3334, svc_chat)
-    var bob = testComm("bob", 3334, 3333, svc_chat)
+    it('should add messages to its queue', function(done){
+        var count = 0;
+        var alice, bob;
+        var started = function() {
+            count++;
+            if (count == 2) { // only now both are started.
+            
+                // connect them 
+                alice.setConnect("localhost", bob.bind.port, "bob");
+                bob.setConnect("localhost", alice.bind.port, "alice");
+                
+                // send a message            
+                for (var i = 0; i < 5; i++) {
+                    alice.sendMessage({'message': 'send', 'args': [ "hi bob " + i ]});
+                    bob.sendMessage({'message': 'send', 'args': [ "hi alice " + i ]});
+                }
 
-    for (var i = 0; i < 5; i++) {
-      alice.sendMessage({'message': 'send', 'args': [ "hi bob " + i ]})
-      bob.sendMessage({'message': 'send', 'args': [ "hi alice " + i ]})
-    }
-
-    setTimeout( // send/receive is async, so just testing after a timeout
-      function() {
-        // FIXME:
-        // Note: we're going under the hood here, actually we should verify the
-        // result by getting /poll. (OTOH, relative merits of writing a bug-compatible
-        // /poll response parser are debateable)
-        for (var i = 0; i < 5; i++) {
-          assert.equal("hi alice " + i, alice.queues['send'][String(i+1)]['msg'])
-          assert.equal("hi bob " + i, bob.queues['send'][String(i+1)]['msg'])
-        }
-        done()
-      },
-      500
-    )
-
-  })
+                // send/receive is async, so just testing after a timeout
+                setTimeout( function() {
+                    // FIXME:
+                    // Note: we're going under the hood here, actually we should verify the
+                    // result by getting /poll. (OTOH, relative merits of writing a bug-compatible
+                    // /poll response parser are debateable)
+                    for (var i = 0; i < 5; i++) {
+                        assert.equal("hi alice " + i, alice.queues['send'][String(i+1)]['msg']);
+                        assert.equal("hi bob " + i, bob.queues['send'][String(i+1)]['msg']);
+                    }
+                    done();
+                  }, 500);
+            }
+        };
+        
+        alice = testComm("alice", svc_chat, started);
+        bob = testComm("bob", svc_chat, started);
+    });
+  
+    it('should allow downloading the scratch extension spec', function (done){
+        done();
+    });
 })
